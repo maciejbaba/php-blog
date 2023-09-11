2408ae387d1d22efadd16ee9c70897606abd73a1
Maciej Bąba <maciek2baba2@gmail.com>
2023-09-11 21:01:31 +0200

    add tailwindcss

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <title>Main Layout</title>
   </head>
