a58310eaf29ec174246f575b55448794fd6fa718
Maciej Bąba <maciek2baba2@gmail.com>
2023-09-11 20:56:49 +0200

    format

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<title>Main View</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <title>Main View</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
 
-<!-- Header Section -->
-<header>
+  <!-- Header Section -->
+  <header>
     <nav>
-        <!-- Your navigation items go here -->
-        <a href="{{ url('/') }}">Home</a>
-        <a href="{{ url('/newview') }}">New View</a>
+      <!-- Your navigation items go here -->
+      <a href="{{ url('/') }}">Home</a>
+      <a href="{{ url('/newview') }}">New View</a>
     </nav>
-</header>
+  </header>
 
-<!-- Content Section -->
-<div class="content">
+  <!-- Content Section -->
+  <div class="content">
     @yield('content')
-</div>
+  </div>
 
-<!-- Footer Section -->
-<footer>
+  <!-- Footer Section -->
+  <footer>
     <p>Footer content here</p>
-</footer>
+  </footer>
 
-<script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
